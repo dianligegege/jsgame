9f6380b48df5ec58c6069c438eb3ee1c82c2e0ac
dianligegege <1220671124@qq.com>
2018-08-18 17:30:21 +0800

    开始爆炸

--- js/ground.js
+++ js/ground.js
@@ -1,9 +1,53 @@
 function Ground(outbox){
 	this.b2=can1.getContext('2d');
+	this.box1=new Image();
 	this.outbox=outbox;
+	this.width=600;
+	this.height=520;
+	this.boxwidth=40;
+	this.boxheight=45;
+	this.loca={dx:0,dy:0,dw:40,dh:45};
+
+	this.boxmap=[
+		[0,0,1,1,0,0,0,1,0,0,0,1,1,1,1],//1
+		[0,1,1,0,2,2,2,0,2,2,2,0,1,1,1],//2
+		[1,1,0,2,0,1,0,2,0,1,0,2,0,1,1],//3
+		[1,0,2,0,1,1,1,1,1,1,1,0,2,0,1],//4
+		[1,0,2,1,1,1,1,1,1,1,1,1,2,0,1],//5
+		[1,0,2,0,1,1,1,1,1,1,1,0,2,0,1],//6
+		[1,0,2,1,1,1,1,1,1,1,1,1,2,0,1],//7
+		[1,1,0,2,0,1,1,1,1,1,0,2,0,1,1],//8
+		[1,1,1,0,2,1,1,1,1,1,2,0,1,1,1],//9
+		[1,1,1,1,0,2,0,1,0,2,0,1,1,1,1],//10
+		[1,1,0,1,1,0,2,2,2,0,1,1,0,1,1],//11
+		[1,0,0,1,1,1,0,0,0,1,1,1,0,0,1],//12
+		[1,0,0,1,1,1,1,1,1,1,1,1,0,0,1]//13
+	]
 }
 
-Ground.prototype.floor= function (){
+Ground.prototype.box= function (){
 	var This = this;
-	// This.outbox.backgroundImage="url(img/floor2.jpg)";
-}
+	for(var n=0;n<13;n++){
+		for(var i=0;i<15;i++){
+			if(This.boxmap[n][i]==1){
+				//创建box1
+				var box1=document.createElement('img');
+				box1.classList.add('img1');
+				box1.src='img/box1.jpg';
+				box1.style.top=n*40+"px";
+				box1.style.left=i*40+"px";
+				This.outbox.appendChild(box1);
+				// This.manobj.b1.drawImage(This.box1, This.loca.dx+40*i, This.loca.dy+40*n, This.loca.dw, This.loca.dh);
+				// This.b2.drawImage(This.box1, This.loca.dx+40*i, This.loca.dy+40*n, This.loca.dw, This.loca.dh);
+			}else if(This.boxmap[n][i]==2) {
+				var box2=document.createElement('img');
+				box2.classList.add('img1');
+				box2.src='img/box2.jpg';
+				box2.style.top=n*40+"px";
+				box2.style.left=i*40+"px";
+				This.outbox.appendChild(box2);
+			}
+		}
+	}
+	
+};
--- js/man.js
+++ js/man.js
@@ -1,58 +1,40 @@
-function Man(){
-	
-	// this.can1 = document.querySelector(can1);
-	console.log(can1);
+function Man(outbox,groundobj) {
 	this.b1 = can1.getContext('2d');
-	this.n=0;
-	this.direction=1;
-	this.speed=8;
-	this.width=600;
-	this.height=600;
-	this.man=new Image();
+	this.n = 0;
+	this.direction = 1;
+	this.speed = 8;//五分之一个动作走的距离
+	this.oncetime=50;//整个跳跃动作所需时间
+	this.width = 600;
+	this.height = 520;
+	this.man = new Image();
+	this.groundobj=groundobj;
+	this.outbox=outbox;
+	this.livebox=document.querySelectorAll(".img1");//所有盒子
+	this.switch1=0;
+	this.boomer=document.createElement('img');
 }
 
-Man.prototype.move=function (){
+Man.prototype.move = function() {
+
+	var This = this;
+	// var switch1=0;
 	
-	var This=this;
-// 	
-// 	setInterval(()=>{
-// 		//清除历史位置
-// 		This.b1.clearRect(This.loca.dx, This.loca.dy, This.loca.dw, This.loca.dh);
-// 		//往不同的方向走
-// 		switch (This.direction) {
-// 			case 0:
-// 				This.loca.dy += This.speed;
-// 				//走到底就回头往上走
-// 				if(This.loca.dy > This.height - This.loca.dh){
-// 					This.direction = 3;
-// 				}
-// 				break;
-// 			case 1:
-// 				This.loca.dx -= This.speed;
-// 				if(This.loca.dx < 0){
-// 					This.direction = 2;
-// 				}
-// 				break;
-// 			case 2:
-// 				This.loca.dx += This.speed;
-// 				if(This.loca.dx > This.width - This.loca.dw){
-// 					This.direction = 1;
-// 				}
-// 				break;
-// 				case 3:
-// 					This.loca.dy -= This.speed;
-// 					if(This.loca.dy  < 0){
-// 						This.direction = 0;
-// 					}
-// 				break;
-// 			default:
-// 				break;
-// 		}
-// 		This.b1.drawImage(This.man, (This.n++%4)*This.loca.sw, This.loca.sh*This.direction, This.loca.sw, This.loca.sh, This.loca.dx, This.loca.dy, This.loca.dw, This.loca.dh);
-// 	},100);
+	//画图
+	function drawimage (sx,sy){
+		This.b1.drawImage(This.man, sx,sy, This.loca.sw, This.loca.sh, This.loca.dx, This.loca.dy, This.loca.dw, This.loca.dh);
+	}
 	
-	This.man.onload = function () {
-		window.onkeyup = function (e) {
+	This.man.onload= function () {
+		drawimage(0,0);
+		window.onkeyup = function() {
+			//判断此时是否在运动,若在运动则按键无效
+			if(This.switch1 == 0) {
+				This.switch1 = 1;
+				moving(event);
+			}
+		};
+		//开始运动
+		function moving(e) {
 			//当用户按不同方向键的时候，裁剪不同的行
 			e.preventDefault();
 			let code = e.keyCode;
@@ -62,89 +44,168 @@ Man.prototype.move=function (){
 			dirArr[39] = 2;
 			dirArr[40] = 0;
 			This.direction = (typeof dirArr[code] == 'number') ? dirArr[code] : This.direction;
-			
+
 			//往不同的方向走
-			switch (This.direction) {
-							case 0://向下走
-								This.b1.clearRect(This.loca.dx, This.loca.dy, This.loca.dw, This.loca.dh);
-								action();
-								// This.loca.dy += This.speed/5;
-								// console.log(action)
-								//走到底就回头往上走
-								if(This.loca.dy > This.height - This.loca.dh){
-									This.direction = 3;
-								}
-								break;
-							case 1://向左走
-								This.b1.clearRect(This.loca.dx, This.loca.dy, This.loca.dw, This.loca.dh);
-								// This.loca.dx -= This.speed;
-								action();
+			switch(This.direction) {
+				case 0: //向下走
+					This.b1.clearRect(This.loca.dx, This.loca.dy, This.loca.dw, This.loca.dh);
+					//判断是否撞箱子
+					var duicuo1;
+					for (var n = 0; n < This.livebox.length; n++) {
+						if (This.livebox[n].offsetTop == This.loca.dy+This.speed*5 && This.livebox[n].offsetLeft == This.loca.dx||This.loca.dy==This.height+40) {
+							duicuo1 = 1;
+							break;
+						} else {
+							duicuo1 = 0;
+						}
+					}
+					if(duicuo1==1){
+						This.switch1 = 0;
+						drawimage(0,0);
+					} else if(duicuo1==0) {
+						action();
+					}
+					break;
+				case 1: //向左走
+					This.b1.clearRect(This.loca.dx, This.loca.dy, This.loca.dw, This.loca.dh);
+					//判断是否撞箱子
+					var duicuo2;
+					for (var n = 0; n < This.livebox.length; n++) {
+						if (This.livebox[n].offsetTop == This.loca.dy  && This.livebox[n].offsetLeft == This.loca.dx-This.speed*5||This.loca.dx==0) {
+							duicuo2 = 1;
+							break;
+						} else {
+							duicuo2 = 0;
+						}
+					}
+					if(duicuo2==1){
+						This.switch1 = 0;
+						drawimage(0,24);
+
+					} else if(duicuo2==0) {
+						action();
+					}
+					break;
+				case 2: //向右走
+					This.b1.clearRect(This.loca.dx, This.loca.dy, This.loca.dw, This.loca.dh);
+					//判断是否撞箱子
+					var duicuo3;
+					for (var n = 0; n < This.livebox.length; n++) {
+						if (This.livebox[n].offsetTop == This.loca.dy  && This.livebox[n].offsetLeft == This.loca.dx+This.speed*5||This.loca.dx==This.width-40) {
+							duicuo3 = 1;
+							break;
+						} else {
+							duicuo3 = 0;
+						}
+					}
+					if(duicuo3==1){
+						This.switch1 = 0;
+						drawimage(0,48);
+
+					} else if(duicuo3==0) {
+						action();
+					}
+					break;
+				case 3: //向上走
+					This.b1.clearRect(This.loca.dx, This.loca.dy, This.loca.dw, This.loca.dh);
+					//判断是否撞箱子
+					var duicuo4;
+					for (var n = 0; n < This.livebox.length; n++) {
+						if (This.livebox[n].offsetTop == This.loca.dy-This.speed*5  && This.livebox[n].offsetLeft == This.loca.dx||This.loca.dy==0) {
+							duicuo4 = 1;
+							break;
+						} else {
+							duicuo4 = 0;
+
+						}
+					}
+					if(duicuo4==1){
+						This.switch1 = 0;
+						drawimage(0,72);
+
+					} else if(duicuo4==0) {
+						action();
+					}
+
+					break;
+				default:
+					break;
+			}
 			
-								if(This.loca.dx < 0){
-									This.direction = 2;
-								}
+			//跳跃动作
+			function action() {
+				var actime = setInterval(function() {
+					//完成五分之一X5的动作
+					if(This.n < 5) {
+						//清除原来位置
+						This.b1.clearRect(This.loca.dx, This.loca.dy, This.loca.dw, This.loca.dh);
+						//判断健值并加减位置
+						switch(This.direction) {
+							case 0:
+								This.loca.dy += This.speed;
 								break;
-							case 2://向右走
-								This.b1.clearRect(This.loca.dx, This.loca.dy, This.loca.dw, This.loca.dh);
-								// This.loca.dx += This.speed;
-								action();
-			
-								if(This.loca.dx > This.width - This.loca.dw){
-									This.direction = 1;
-								}
+							case 1:
+								This.loca.dx -= This.speed;
 								break;
-							case 3://向上走
-								This.b1.clearRect(This.loca.dx, This.loca.dy, This.loca.dw, This.loca.dh);
-								// This.loca.dy -= This.speed;
-								action();
-			
-								if(This.loca.dy  < 0){
-									This.direction = 0;
-								}
+							case 2:
+								This.loca.dx += This.speed;
+								break;
+							case 3:
+								This.loca.dy -= This.speed;
 								break;
 							default:
 								break;
 						}
-			
-			//跳跃动作
-			function action(){
-				console.log(123)
-					var actime=setInterval(function(){
-						//完成四分之一的动作
-						if(This.n<5){
-
-							//清除原来位置
-							This.b1.clearRect(This.loca.dx, This.loca.dy, This.loca.dw, This.loca.dh);
-							
-							switch (This.direction){
-								case 0:	This.loca.dy += This.speed;
-									break;
-								case 1:	This.loca.dx -= This.speed;
-									break;
-								case 2:	This.loca.dx += This.speed;
-									break;
-								case 3:	This.loca.dy -= This.speed;
-									break;
-								default:
-									break;
-							}
-
-							This.b1.drawImage(This.man, (This.n++%4)*This.loca.sw, This.loca.sh*This.direction, This.loca.sw, This.loca.sh, This.loca.dx, This.loca.dy, This.loca.dw, This.loca.dh);
-						} else{
-							clearInterval(actime);
-							This.n=0;
-						};
-					},100);
+						drawimage((This.n++ % 4) * This.loca.sw,This.loca.sh * This.direction);
+					} else {
+						clearInterval(actime);
+						This.n = 0;
+						This.switch1=0;
+					};
+				}, This.oncetime);
 			}
-			
-			
-		
-			
-			
-		
+
 		}
 	}
 }
 
-            
+Man.prototype.boom = function(){
+	var This = this;
+	window.onkeydown=function(event){
+		//当跳跃动作完成后可以放炸弹
+		if(This.switch1==0){
+			var code=event.keyCode;
+			if(code==13){
+				console.log(This.loca.dx, This.loca.dy)
+				// var boomer=document.createElement('img');
+				This.boomer.classList.add('boomer');
+				//在这里加放大变小动画
+				This.boomer.src='img/boomer.png';
+				This.boomer.style.top=This.loca.dy+"px";
+				This.boomer.style.left=This.loca.dx+"px";
+				This.outbox.appendChild(This.boomer);
+				
+				//定时爆炸
+				setTimeout(function(){
+					//先爆炸
+					//连环爆炸
+					fire();
+					//再消失
+					setTimeout(function(){
+						This.boomer.remove();
+					},300)
+				},2000)
 
+			}
+		}
+		
+
+	}
+	
+	//连环爆炸
+	function fire(){
+		//炸弹先爆炸
+		This.boomer.src='img/boom.png';
+
+	}
+}
